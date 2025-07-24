d86daf03bb64263de556e9e9b986f0036cd82715
Esteban A. <efarenas@gmail.com>
2025-07-24 16:03:33 -0400

    LTI de prueba

---
+++ lti-success/page.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Éxito LTI</title>
+</head>
+<body>
+    <h1>¡Conexión LTI exitosa!</h1>
+    <p>Moodle se ha comunicado correctamente con Vercel.</p>
+</body>
+</html>
